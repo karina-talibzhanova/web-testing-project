74adfd275ebaa8eaaf4255b38f919a8f5e1b9e5f
karina-talibzhanova <karina.talibzhanova@gmail.com>
2021-11-11 14:07:14 +0000

    now using environment variables!

--- src/test/java/framework/cucumber/stepdefs/BuyProductsStepDefs.java
+++ src/test/java/framework/cucumber/stepdefs/BuyProductsStepDefs.java
@@ -21,11 +21,11 @@ public class BuyProductsStepDefs {
 
     @Before
     public void setup(){
-        Util.setDriverLocation("src/test/resources/chromedriver.exe");
+        Util.setDriverLocation(System.getenv("ChromeDriverPath"));
         webDriver= new ChromeDriver();
         Login loginPage= new LoginPage(webDriver); //starts from login page (makes sense because we always need to login)
         loginPage.inputName("standard_user");
-        loginPage.inputPass("password");
+        loginPage.inputPass("secret_sauce");
         inventoryPage = loginPage.clickLogin();
     }
 
