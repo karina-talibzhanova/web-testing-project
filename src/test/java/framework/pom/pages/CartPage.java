86e00307fd83769d2c6c89de56357fc32e69159a
Kevin <48499023+KevinSimbana@users.noreply.github.com>
2021-11-11 12:20:44 +0000

    Implemented removeProducts method

--- src/test/java/framework/pom/pages/CartPage.java
+++ src/test/java/framework/pom/pages/CartPage.java
@@ -3,19 +3,24 @@ package framework.pom.pages;
 import framework.pom.interfaces.Cart;
 import framework.pom.interfaces.Inventory;
 import framework.pom.interfaces.Product;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 public class CartPage extends CommonPage implements Cart {
+    private WebDriver webDriver;
     public CartPage(WebDriver webDriver) {
         super(webDriver);
     }
 
     @Override
     public void removeProducts(WebElement product) {
-
+        WebElement button = product.findElement(By.tagName("button"));
+        if(button.getText().equals("Remove")) {
+            button.click();
+        }
     }
 
     @Override
