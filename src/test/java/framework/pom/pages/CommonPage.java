02523fdab11ba037fff5ba29ff7f9c2ceff354a8
TobiOl <1712264@brunel.ac.uk>
2021-11-11 15:18:01 +0000

    new change

--- src/test/java/framework/pom/pages/CommonPage.java
+++ src/test/java/framework/pom/pages/CommonPage.java
@@ -73,7 +73,7 @@ public abstract class CommonPage implements CommonPageInterface {
 
     public Cart goToCartPage() {
         return null;
-    };
+    }
 
     public String getUrl(){
         return webDriver.getCurrentUrl();
